32570f47e20ddd9295d4a54840ee617fce1643bf
in kyung <kik87827@gmail.com>
2023-08-10 22:49:42 +0900

    신규페이지 세팅

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -254,8 +254,8 @@ function comboFunc() {
       } else {
         option_group.setAttribute("data-option", thisElement.getAttribute("id"));
       }
-      if (element.closest(".fullpop_contlow") !== null) {
-        element.closest(".fullpop_contlow").appendChild(option_group);
+      if (element.closest(".popup_content_low") !== null) {
+        element.closest(".popup_content_low").appendChild(option_group);
       } else {
         appBody.appendChild(option_group);
       }
@@ -365,3 +365,143 @@ function bottomFoldFunc(){
     }
   });
 }
+
+
+/* popup */
+
+/**
+ * 디자인 팝업
+ * @param {*} option 
+ */
+function DesignPopup(option) {
+  this.option = option;
+  this.selector = this.option.selector;
+
+  if (this.selector !== undefined) {
+    this.selector = document.querySelector(this.option.selector);
+  }
+  this.design_popup_wrap = document.querySelectorAll(".popup_wrap");
+  this.domHtml = document.querySelector("html");
+  this.domBody = document.querySelector("body");
+  this.pagewrap = document.querySelector(".page_wrap");
+  this.layer_wrap_parent = null;
+  this.btn_closeTrigger = null;
+  this.btn_close = null;
+  this.bg_design_popup = null;
+  this.scrollValue = 0;
+
+  const popupGroupCreate = document.createElement("div");
+  popupGroupCreate.classList.add("layer_wrap_parent");
+
+  if(!this.layer_wrap_parent && !document.querySelector(".layer_wrap_parent")){
+    this.domBody.append(popupGroupCreate);
+  }
+
+  this.layer_wrap_parent = document.querySelector(".layer_wrap_parent");
+
+  
+
+  // console.log(this.selector.querySelectorAll(".close_trigger"));
+
+
+
+  this.bindEvent();
+}
+
+
+
+DesignPopup.prototype.dimCheck = function(){
+  const popupActive = document.querySelectorAll(".popup_wrap.active");
+  if(!!popupActive[0]){
+    popupActive[0].classList.add("active_first");
+  }
+  if(popupActive.length>1){
+    this.layer_wrap_parent.classList.add("has_active_multi");
+  }else{
+    this.layer_wrap_parent.classList.remove("has_active_multi");
+  }
+}
+DesignPopup.prototype.popupShow = function () {
+  this.design_popup_wrap_active = document.querySelectorAll(".popup_wrap.active");
+
+  if (this.selector == null) {
+    return;
+  }
+  this.domHtml.classList.add("touchDis");
+  
+  this.selector.classList.add("active");
+  setTimeout(()=>{
+    this.selector.classList.add("motion_end");
+  },420);
+  if ("beforeCallback" in this.option) {
+    this.option.beforeCallback();
+  }
+
+  if ("callback" in this.option) {
+    this.option.callback();
+  }
+  if(!!this.design_popup_wrap_active){
+    this.design_popup_wrap_active.forEach((element,index)=>{
+      if(this.design_popup_wrap_active !== this.selector){
+        element.classList.remove("active");
+      }
+    })
+  }
+  //animateIng = true;
+
+  this.layer_wrap_parent.append(this.selector);
+  
+
+  this.dimCheck();
+
+  // this.layer_wrap_parent
+
+  // ****** 주소 해시 설정 ****** //
+  // location.hash = this.selector.id
+  // modalCount++
+  // modalHash[modalCount] = '#' + this.selector.id
+}
+DesignPopup.prototype.popupHide = function () {
+  var target = this.option.selector;
+  if (target !== undefined) {
+
+    this.selector.classList.remove("motion");
+    if ("beforeClose" in this.option) {
+      this.option.beforeClose();
+    }
+     //remove
+     this.selector.classList.remove("motion_end");
+     setTimeout(()=>{
+
+       this.selector.classList.remove("active");
+     },420)
+     this.design_popup_wrap_active = document.querySelectorAll(".popup_wrap.active");
+     this.dimCheck();
+     if ("closeCallback" in this.option) {
+       this.option.closeCallback();
+     }
+     if (this.design_popup_wrap_active.length == 0) {
+       this.domHtml.classList.remove("touchDis");
+     }
+  }
+}
+
+DesignPopup.prototype.bindEvent = function () {
+  this.btn_closeTrigger = this.selector.querySelectorAll(".close_trigger,.btn_closeitem");
+  this.btn_close = this.selector.querySelectorAll(".btn_popup_close");
+  this.bg_design_popup = this.selector.querySelector(".bg_dim");
+
+  var closeItemArray = [...this.btn_closeTrigger, ...this.btn_close];
+
+  if (this.bg_design_popup !== null) {
+    closeItemArray.push(this.bg_design_popup);
+  }
+  if (closeItemArray.length) {
+    closeItemArray.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        e.preventDefault();
+        this.popupHide(this.selector);
+      }, false);
+    });
+  }
+};
